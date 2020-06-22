9d1d4b33fd053931397249de609c7d250acbee53
Thiago Teixeira <Thiago.Teixeira-ext@ab-Inbev.com>
2020-06-22 13:23:36 -0300

    Adjusting path

--- assets/js/search.js
+++ assets/js/search.js
@@ -44,7 +44,7 @@ $(function () {
   var m = $("meta[name=baseurl]");
 
   // $.getJSON(m.attr("content") + "/search.json")
-  $.getJSON($("baseurl/search.json"))
+  $.getJSON("/search.json")
     .done(function (data) {
       if (data.code == 0) {
         $("#search-content").typeahead({
